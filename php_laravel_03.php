71e60e0a5a2cadde730b0d46ea038675e1ed7083
Hiroto Nishidate <tak713tak@gmail.com>
2020-06-03 08:22:59 +0000

    first commit

---
+++ php_laravel_03.php
@@ -0,0 +1,44 @@
+<?php
+
+//課題1
+$name = 'hiroto_nishidate';
+if($name == 'hiroto_nishidate'){
+    echo '私はあなたの名前です';
+    echo "\n";
+}else{
+    echo '私はあなたの名前ではありません';
+    echo "\n";
+}    
+
+
+//課題2
+$total = 0;
+
+for ($i = 0; $i <= 10000; $i++) {
+    $total += $i;
+}
+echo $total;
+echo "\n";
+
+//課題3
+$fruits = array("Orange","Apple","Strawberry","Grape","Banana");
+foreach($fruits as $Fruits){
+    echo $Fruits;
+    echo "\n";
+}
+
+//応用課題
+$start = 1;
+$end = 100;
+
+for($i = $start; $i <= $end; $i++) {
+    if($i % 5 == 0) {
+        echo $i;
+        echo "\n";
+    }
+}
+
+
+?>
+
+
